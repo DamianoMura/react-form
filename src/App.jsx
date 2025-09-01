3afbb963cce61c4f50b02c646e1bfbc4c2f1596f
DamianoMura <mura.damiano14@gmail.com>
2025-09-01 09:46:58 +0200

    [add] article list created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-
+const articles = [
+  {
+    id : 1,
+    title: "articolo 1",
+    description : "descrizione articolo 1"
+  },
+  {
+    id : 2,
+    title: "articolo 2",
+    description : "descrizione articolo 2"
+  },
+  {
+    id : 3,
+    title: "articolo 3",
+    description : "descrizione articolo 3"
+  },
+  {
+    id : 4,
+    title: "articolo 4",
+    description : "descrizione articolo 4"
+  },
+  {
+    id : 5,
+    title: "articolo 5",
+    description : "descrizione articolo 5"
+  },
+  {
+    id : 6,
+    title: "articolo 6",
+    description : "descrizione articolo 6"
+  },
+]
 
 function App() {
   
 
   return (
     <>
-      
+      <div className="container">
+        <h1>lista articoli</h1>
+      </div>
     </>
   )
 }
