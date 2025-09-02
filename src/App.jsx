d742a621ac85255a51498a741358efddf2c037cc
DamianoMura <mura.damiano14@gmail.com>
2025-09-02 16:36:13 +0200

    [upd] base layout with list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const articles = [
   {
     id : 1,
@@ -32,10 +34,10 @@ const articles = [
 ]
 
 function App() {
+  const [newArticle , setNewArticle] = useState("");
   
-
   return (
-    <>
+   
       <div className="container">
         <div className="row">
           <div className="col-12">
@@ -48,14 +50,42 @@ function App() {
 
                 <li className="list-group-item" key={article.id}>
                   <h3>{article.title}</h3>
+                  <div className="btn-danger"></div>
                 </li>
                 )
               })}
             </ul>
+
           </div>
+          <div className="col-12">
+            
+                <div className="card">
+                  <div className="card-header">
+                    <h3>aggiungi articolo</h3>
+                  </div>
+                  <div className="card-body">
+                    <input 
+                      type="text" 
+                      placeholder="new title"
+                      
+                    /> 
+                    
+                  <div className="card-footer">
+                    <button 
+                      className="btn btn-success
+                    ">
+                      inserisci
+                    </button>
+                  </div>
+                </div>               
+                
+              
+          </div>
+        
         </div>
       </div>
-    </>
+    </div>
+    
   )
 }
 
